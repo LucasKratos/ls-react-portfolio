9a58418847ab012dadf531930342e2a36a643530
LucasKratos <lucas.spagnoli@gmail.com>
2022-06-21 14:13:54 -0300

    4

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     to-gray-800 p-5 
      text-white '>
         <div className='flex flex-col p-4 justify-center 
-        max-w-screen-lg mx-auto sm: mt-10 '>
+        max-w-screen-lg mx-auto sm: mt-40 '>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 
                 border-gray-500'>Contact</p>
@@ -20,19 +20,19 @@ const Contact = () => {
                     type="text" 
                     name='name' 
                     placeholder='Enter your name' 
-                    className='p-2 bg-transparent border-2 rounded-md 
+                    className='p-2 bg-black border-2 rounded-md 
                     text-white focus:outline-none' 
                 />
                  <input 
                     type="text" 
                     name='email' 
                     placeholder='Enter your Email' 
-                    className='my-4 p-2 bg-transparent border-2 rounded-md 
+                    className='my-4 p-2 bg-black border-2 rounded-md 
                     text-white focus:outline-none' 
                 />
                 <textarea name="message" rows="10"
                     placeholder='Enter yout message'
-                    className='p-2 bg-transparent border-2 rounded-md 
+                    className='p-2 bg-black border-2 rounded-md 
                     text-white focus:outline-none'></textarea>
 
                 <button className='text-white bg-gradient-to-b 
