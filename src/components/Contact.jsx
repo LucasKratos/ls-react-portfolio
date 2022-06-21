56e355c3dad5fee6472b64c152f6c787d0f660b0
LucasKratos <lucas.spagnoli@gmail.com>
2022-06-21 14:08:06 -0300

    3

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     to-gray-800 p-5 
      text-white '>
         <div className='flex flex-col p-4 justify-center 
-        max-w-screen-lg mx-auto  '>
+        max-w-screen-lg mx-auto sm: mt-10 '>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 
                 border-gray-500'>Contact</p>
